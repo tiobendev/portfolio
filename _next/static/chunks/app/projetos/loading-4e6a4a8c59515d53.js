d0781f40fa9fb61bc07b8416a82770acb627ad96
tiobendev <brenofreitasdev@gmail.com>
2024-07-04 17:41:03 -0300

    Atualizando engines no package.json

---
+++ _next/static/chunks/app/projetos/loading-4e6a4a8c59515d53.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[939,409,404,115,725,239,92],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
